9a59c4c4bed20a3504441588c326bb2cb61c3181
JorgeWebsec <jorgewebsec@gmail.com>
2016-11-11 20:21:44 +0100

    add function insert_mongodb

--- suneo-bot.py
+++ suneo-bot.py
@@ -41,6 +41,9 @@ class colores:
 client = MongoClient()
 db = client.test
 
+client = MongoClient()
+db = client.test
+
 TAG_RE = re.compile(r'<[^>]+>')
 def remove_tags(text):
     return TAG_RE.sub('', text)
@@ -61,36 +64,42 @@ def get_html_without_subdomain(target):
 	except urllib2.URLError, e:
 		return "False"
 
+def get_html(target):
+	url = "http://" + str(target)
+	print colores.verde + "[INFO] Generate URL: " + colores.normal + str(url)
+	opener = urllib2.build_opener()
+	opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
+	html = opener.open('http://www.stackoverflow.com').read()
+	return html
+
 def detect_wp(html, dominio):
 	soup = BeautifulSoup(html, "html.parser")
-	try:	
-		#Buscamos generator
-		gen = soup.findAll(attrs={"name":"generator"})
-		if "Wordpress" in str(gen):
+	#Buscamos wp-content en el html
+	try:
+		if html.find("wp-content")>0:
 			return True
-		else: #Buscamos wp-content en el html
-			if html.find("wp-content")>0:
-				return True
-			else:#Buscamos links con xmlrpc.php
-				links = soup.findAll("link")
-				for l in links:
-					if "xmlrpc.php" in str(l):
-						return True
-					else:#Buscamos el readme.html
-						try:
-							url = "http://" + dominio + "/readme.html"
-							html = urllib.urlopen(url).read()
-							soup = BeautifulSoup(html)
-							for h1 in soup.find_all('h1', {'id':"logo"}):
-								h1 = remove_tags(str(h1)) #PARSER
-								if h1:
-									return True
-						except urllib2.HTTPError, e:
-							continue 
-						except urllib2.URLError, e:
-							continue
-						except httplib.HTTPException, e:
-							continue
+		else:
+	#Buscamos links con xmlrpc.php
+			links = soup.findAll("link")
+			for l in links:
+				if "xmlrpc.php" in str(l):
+					return True
+				else:
+	#Buscamos el readme.html
+					try:
+						url = "http://" + dominio + "/readme.html"
+						html = urllib.urlopen(url).read()
+						soup = BeautifulSoup(html)
+						for h1 in soup.find_all('h1', {'id':"logo"}):
+							h1 = remove_tags(str(h1)) #PARSER
+							if h1:
+								return True
+					except urllib2.HTTPError, e:
+						continue 
+					except urllib2.URLError, e:
+						continue
+					except httplib.HTTPException, e:
+						continue
 	except:
 		return False
 
@@ -114,6 +123,17 @@ def detect_drupal(html):
 	except:
 		return False
 
+def insert_mongodb(cms, dominio, IP):
+	try:
+		client = MongoClient()
+		db = client.test
+		date_Insert = time.strftime("%H:%M:%S")
+		date_Update = ""
+		cursor = db.Shodita.insert({"ip":IP, "dominio":dominio, "cms":cms, "bot":"Suneo", "date_insert": date_Insert, "date_Update": date_Update})
+		print colores.azul + "[INFO] INSERT IN DB" + colores.normal
+	except:
+		print "[WARNING]ERROR INSERT MONGODB"
+
 def get_target():
 	global client, db
 	cursor = db.Shodita.find({"bot":"Shizuka"})
@@ -130,10 +150,13 @@ def get_target():
 				if response.code == 200 or response.code == "OK":
 					html = response.read()
 					if detect_wp(html, document["dominio"]) == True:
+						insert_mongodb("WordPress", document["dominio"], document["ip"])
 						print colores.verde + "[+][INFO] " + document["dominio"] + " is WordPress" + colores.normal
 					if detect_joomla(html):
+						insert_mongodb("Joomla", document["dominio"], document["ip"])
 						print colores.verde + "[+][INFO] " + document["dominio"] + " is Joomla" + colores.normal
 					if detect_drupal(html):
+						insert_mongodb("Drupal", document["dominio"], document["ip"])
 						print colores.verde + "[+][INFO] " + document["dominio"] + " is Drupal" + colores.normal
 			except URLError, e:
 				continue
@@ -141,7 +164,9 @@ def get_target():
 				continue
 			except:
 				continue
+
 def main():
 	get_target()
+
 if __name__ == '__main__':  
 	main()
